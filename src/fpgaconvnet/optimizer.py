2b8d4bde8864cefc693c85b2281b8bc10cbcc2f6
Fu Yong Quah <quah.fy95@gmail.com>
2017-02-24 23:54:15 +0000

    Add GP and linear model code for optimizer.

--- src/fpgaconvnet/optimizer.py
+++ src/fpgaconvnet/optimizer.py
@@ -1,21 +1,23 @@
 import argparse
 import sys
 
-from fpgaconvnet.protos import parameters_pb2
 
+import GPy
+from mpl_toolkits import mplot3d
+from matplotlib import pyplot as plt
 import numpy as np
 from scipy import optimize
 from sklearn import linear_model
+import yaml
 
 
 parser = argparse.ArgumentParser(
         description="Takes a CNN design (in fpgaconvnet.protos.Paramter.Network"
                     " protos form) and produces the ideal factor allocations "
                     " subject to resource constraints.")
-parser.add_argument("design", type=str, nargs="",
+parser.add_argument("design", type=str,
                     help="Path to the protos with the design. *_folding_factor"
-                         " fields in the proto will be ignored.",
-                    required=True)
+                         " fields in the proto will be ignored.")
 parser.add_argument("--resource-bench", dest="resource_bench", type=str,
                     help=("Path to yaml file with the results from running "
                           " resource benches."),
@@ -23,139 +25,105 @@ parser.add_argument("--resource-bench", dest="resource_bench", type=str,
 parser.add_argument("--output", dest="output", type=str,
                     help="Path to the protos with the optimized output.",
                     required=True)
-                     
-
-def make_constraint(func):
-    """Returns a constraint dict where func(*args) <= 0."""
-    def new_func(v):
-        # v is a numpy array.
-        return -(func(*v))
-    return {"type": "ineq", "fun": new_func}
 
 
-def make_trivial_constraints(n_args):
-    for arg in n_args:
-        yield make_constraint()
+def plot_figs(fig_num, elev, azim, X, Y, clf, labels=["X_1", "X_2", "Y"]):
+    fig = plt.figure(fig_num, figsize=(4, 3))
+    plt.clf()
+    ax = mplot3d.Axes3D(fig, elev=elev, azim=azim)
 
-
-# ===== Simple Constraint, for testing purposes ======
-
-def simple_target(x):
-    return x **2
-
-
-simple_constraints = [
-        make_constraint(lambda x: x + 1)]
-
-
-def make_composite_factors(factors):
-    return np.array([(a, b, a * b) for a,b in factors])
-
-# ===== LeNet ====
-
-CONV_FACTORS = np.array([
-        (50, 25), (50, 5), (50, 1), (20, 25),
-        (20, 1), (10, 25), (10, 5)])
-COMPOSITE_CONV_FACTORS = make_composite_factors(CONV_FACTORS)
-
-
-def _build_linear_models():
-    # These are results obtained from running resource bench - DO NOT CHANGE THEM!
-    flip_flops = np.array(
-            [141265, 141305, 136848, 83962, 79929, 46909, 46441])
-    dsp_blocks = np.array(
-            [750, 150, 30, 500, 20, 250, 50])
-    block_memory = np.array(
-            [1351, 1351, 1351, 920, 920, 989, 989])
-    throughput = np.array(
-            [2034.0, 736.068, 312.48, 959.693, 128.705, 491.359, 158.381])
-    # End of resource_bench results.
-
-    for y in (flip_flops, dsp_blocks, block_memory, throughput):
-        lm = linear_model.LinearRegression()
-        lm.fit(COMPOSITE_CONV_FACTORS, y)
-        yield lm
-
-
-FLIP_FLOP_MODEL, DSP_BLOCK_MODEL, BLOCK_MEMORY_MODEL, THROUGHPUT_MODEL = \
-        tuple(_build_linear_models())
-
-# Use only 80% of LUT, give some buffer for pooling units.
-# We do not need such conservative checks for DSP and Block Memory, as pooling unit
-# do not use those.
-TOTAL_FLIP_FLOPS = 262400 * 0.8
-TOTAL_DSP_BLOCKS = 3926
-TOTAL_BLOCK_MEMORY = 2567
-
-
-def lenet_target(x):
-    ff0, kff0, ff1, kff1 = x
-    # It makes more sense to minimize throughput rather than
-    # multiply them.
-    t0 = THROUGHPUT_MODEL.predict(np.array(
-            [[ff0, kff0, ff0 * kff0]]))[0]
-    t1 = THROUGHPUT_MODEL.predict(np.array(
-            [[ff1, kff1, ff1 * kff1]]))[0]
-    # The ratio of weightage between t0 and t1  should be similar
-    # to the ratio between number of convolutions in the two kernels
-    # Negatate the result, because we are maimizing the function
-    return -t0-50*t1
-
-
-def _make_lenet_constraint_func(model, total):
-    def f(ff0, kff0, ff1, kff1):
-        p0 = model.predict(np.array(
-                [[ff0, kff0, ff0 * kff0]]))[0]
-        p1 = model.predict(np.array(
-                [[ff1, kff1, ff1 * kff1]]))[0]
-        return (p0 + p1 - total)
-    return f
-
-
-block_memory_constraint = _make_lenet_constraint_func(
-        BLOCK_MEMORY_MODEL, TOTAL_BLOCK_MEMORY)
-flip_flop_constraint = _make_lenet_constraint_func(
-        FLIP_FLOP_MODEL, TOTAL_FLIP_FLOPS)
-dsp_block_constraint = _make_lenet_constraint_func(
-        DSP_BLOCK_MODEL, TOTAL_DSP_BLOCKS)
-
-
-lenet_constraints = [make_constraint(func) for func in [
-        block_memory_constraint, flip_flop_constraint,
-        dsp_block_constraint]]
-lenet_bounds = [
-    (1, 20),    # ff0
-    (1, 25),    # kff0
-    (1, 1000),  # ff1
-    (1, 25),    # kff1
-]
-
-
-MAPPINGS = {
-        "lenet": (4, lenet_target, lenet_constraints, lenet_bounds),
-        "simple": (1, simple_target, simple_constraints, [])}
-
-
-def run_optimizer(params_size, target_function, constraints, bounds):
-    initial_vector = [10 for _ in range(params_size)]
-    options = {"disp": True, "maxfev": 100000, "maxiter": 100000}
-    res = optimize.minimize(target_function,
-            x0=initial_vector,
-            constraints=constraints,
-            bounds=bounds,
-            options=options)
-    print(res)
+    ax.scatter(X[:, 0], X[:, 1], Y, c='k', marker='+')
+    X_0_min = np.min(X[:, 0]) - 0.1
+    X_0_max = np.max(X[:, 0]) + 0.1
+    X_1_min = np.min(X[:, 1]) - 0.1
+    X_1_max = np.max(X[:, 1]) + 0.1
+    ax.plot_surface(np.array([[X_0_min, X_0_min],
+                              [X_0_max, X_0_max]]),
+                    np.array([[X_1_min, X_1_max],
+                              [X_1_min, X_1_max]]),
+                    clf.predict(
+                        np.array([[X_0_min, X_1_min],
+                                  [X_0_min, X_1_max],
+                                  [X_0_max, X_1_min],
+                                  [X_0_max, X_1_max]])
+                                ).reshape((2, 2)),
+                    alpha=.5)
+    ax.set_xlabel(labels[0])
+    ax.set_ylabel(labels[1])
+    ax.set_zlabel(labels[2])
+    ax.w_xaxis.set_ticklabels([])
+    ax.w_yaxis.set_ticklabels([])
+    ax.w_zaxis.set_ticklabels([])
 
 
 def main(argv):
-    if len(argv) >= 2:
-        name = argv[1]
-    else:
-        name = "simple"
-        print("No mappings provided, using %s!" % name)
-    params_size, target_function, constraints, bounds = MAPPINGS[name]
-    run_optimizer(params_size, target_function, constraints, bounds)
+    with open(FLAGS.resource_bench, "r") as f:
+        resource_bench = yaml.load(f.read())
+
+    max_logic_utilization = resource_bench["resources"]["logic_utilization"]
+    max_multiplier = resource_bench["resources"]["multiplier"]
+    max_block_memory = resource_bench["resources"]["block_memory"]
+    # Each M20k block contains 20k bits
+
+    X = []
+    Y = []
+
+
+    for datum in resource_bench["data"]:
+        # We do not attempt to model everything automatically, somethings are
+        # simple to model:
+        #
+        #   1. number_of_multipliers - upper bounded by `kernel_size *
+        #      kernel_size`. In some cases the multiplication tree will be
+        #      optimized by Maxeler and uses less resources, but this is
+        #      a good general rule of thumb.
+        #   2. base_number_of_block_memory - At the bare minimum, the layer
+        #      requires `B = (total_weights + total_bias) * bits_per_weight`
+        #      bits of FMem, which translates to at least `B / (20,480)`
+        #      M20 blocks.
+        #
+        # That said, what we try to model with our training data:
+        #   1. logic_utilization
+        #   2. residual_block_memory
+
+        params = datum["params"]
+        layer = datum["layer"]
+        usage = datum["usage"]
+        total_weights = (layer["num_inputs"] * layer["num_outputs"]
+                * layer["kernel_size"] * layer["kernel_size"] + layer["num_outputs"])
+        base_m20k = (total_weights * 18.0) / 20480.0
+
+        wf = float(params["worker_factor"]) \
+                / layer["num_inputs"]
+        cff = float(params["conv_folding_factor"]) \
+                / layer["num_outputs"]
+        kff = float(params["kernel_folding_factor"]) \
+                / (layer["kernel_size"] * layer["kernel_size"])
+        X.append([wf, cff])
+        Y.append([usage["logic_utilization"],
+                  float(usage["block_memory"] - base_m20k)])
+
+    X = np.array(X)
+    Y = np.array(Y)
+    logic_utilization_model = linear_model.LinearRegression(
+            normalize=True, fit_intercept=True)
+    logic_utilization_model.fit(X, Y[:, 0])
+    residual_block_memory_model = linear_model.LinearRegression(
+            normalize=True, fit_intercept=True)
+    residual_block_memory_model.fit(X, Y[:, 0])
+
+    # GP Model - not very useful unless we have a lot of data.
+    # ker = GPy.kern.Poly(input_dim=X.shape[1])
+    # model = GPy.models.GPRegression(X, Y[:, 0:1], ker)
+    # model.optimize(messages=1)
+    # print(model)
+    # print(model.param_array)
+    # _  = model.plot()
+    # plt.show()
+
+
 
 
 if __name__ == "__main__":
+    FLAGS = parser.parse_args()
     main(sys.argv)
