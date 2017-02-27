3d82ac4991cef1d6099287792fedce455a0bc8a2
Fu Yong Quah <quah.fy95@gmail.com>
2017-02-27 13:50:10 +0000

    Add code to run from cpu

--- src/fpgaconvnet/lenet_main.cpp
+++ src/fpgaconvnet/lenet_main.cpp
@@ -76,19 +76,20 @@ std::vector<float> run_feature_extraction(
             "../test_data/lenet/weights/conv2_kernels.txt",
             "../test_data/lenet/weights/conv2_bias.txt"};
     max_file_t *max_file = lenet_init();
+    std::vector<float> extracted_features;
     fpgaconvnet::Convnet convnet(network_parameters, max_file, "");
 
     convnet.load_weights_from_files(filenames);
-    convnet.max_init_weights();
-    convnet.max_load_input_data(images, N);
-    convnet.max_run_inference(N);
-    std::vector<float> extracted_features = convnet.max_retrieve_features(N);
+    // convnet.max_init_weights();
+    // convnet.max_load_input_data(images, N);
+    extracted_features = convnet.max_run_inference(N, images);
+    // std::vector<float> extracted_features = convnet.max_retrieve_features(N);
 
-    fpgaconvnet::verify_conv_output(
-            network_parameters,
-            N,
-            &extracted_features[0],
-            "../test_data/lenet/output.txt");
+    // fpgaconvnet::verify_conv_output(
+    //         network_parameters,
+    //         N,
+    //         &extracted_features[0],
+    //         "../test_data/lenet/output.txt");
 
     return extracted_features;
 }
