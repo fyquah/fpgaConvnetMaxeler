732913f117eabc880af269be6f5e9f3447d60bc3
Fu Yong Quah <quah.fy95@gmail.com>
2017-03-17 18:47:13 +0000

    Modify main.cpp template

--- templates/main.template.cpp
+++ templates/main.template.cpp
@@ -34,6 +34,7 @@ std::vector<float> run_feature_extraction(
 
     /* TODO: load the weights from somewhere. */
     convnet.randomize_weights();
+    convnet.max_init_weights();
 
     /* warm up the DFE with the weights. */
     extracted_features = convnet.max_run_inference(N, images, false);
