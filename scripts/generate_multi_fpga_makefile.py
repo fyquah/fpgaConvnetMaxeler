8ee4d01024c439a8be52c7c819d2c1ccbe5ace45
Fu Yong Quah <quah.fy95@gmail.com>
2017-03-23 22:18:14 +0000

    Add script to create project for multiple FPGA, fix parameters.proto

---
+++ scripts/generate_multi_fpga_makefile.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+from google.protobuf import text_format
+from fpgaconvnet.protos import parameters_pb2
+
+parser.add_argument("descriptor", type=str, help="Name of network descriptor.")
+parser.add_argument("output", type=str, help="Name of the generated Makefile.")
+
+
+def main():
+    network = text_format.Parse(f.read(), parameters_pb2.Network())
+    arr = []
+    for i, layer in enumerate(network.layer):
+        if len(arr) == 0 or arr[-1] != layer.fpga_id:
+            arr.append(layer.fpga_id)
+
+        if not layer.HasField("fpga_id")::
+            raise RuntimeError("Missing fpga_id in layer %d!" % i)
+
+    with open(FLAGS.output, "w") as f:
+        f.write("TARGET_NAMES= " + ["target_%d" % i for i in arr].join(" "))
+
+
+if __name__ == "__main__":
+    FLAGS = parser.parse_args()
+    main()
