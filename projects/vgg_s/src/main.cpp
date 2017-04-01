646362be999ebea3eba97f0a36ae9bbdac81ed13
Fu Yong Quah <quah.fy95@gmail.com>
2017-04-01 19:04:42 +0100

    Load vgg data and weights in main.cpp

--- projects/vgg_s/src/main.cpp
+++ projects/vgg_s/src/main.cpp
@@ -12,7 +12,7 @@
 
 
 #ifdef __SIM__
-    static const uint64_t N = 6;
+    static const uint64_t N = 2;
 #else
     static const uint64_t N = 10000;
 #endif
@@ -28,23 +28,82 @@ std::vector<float> run_feature_extraction(
         const std::vector<float> & images
 )
 {
+    std::vector<std::string> filenames = {
+            "../weights/conv0_weights.bin",
+            "../weights/conv0_bias.bin",
+            "../weights/conv3_weights.bin",
+            "../weights/conv3_bias.bin",
+            "../weights/conv5_weights.bin",
+            "../weights/conv5_bias.bin"
+            "../weights/conv6_weights.bin",
+            "../weights/conv6_bias.bin"
+            "../weights/conv7_weights.bin",
+            "../weights/conv7_bias.bin"
+    };
     max_file_t *max_file = vgg_s_init();
     std::vector<float> extracted_features;
     fpgaconvnet::Convnet convnet(network_parameters, max_file, "");
 
-    /* TODO: load the weights from somewhere. */
-    convnet.randomize_weights();
+    convnet.load_weights_from_files(filenames, fpgaconvnet::FORMAT_BINARY);
     convnet.max_init_weights();
 
     /* warm up the DFE with the weights. */
     extracted_features = convnet.max_run_inference(N, images, false);
     extracted_features = convnet.max_run_inference(N, images, false);
 
-    /* TODO: Verify the output is correct. You can use the
-     * fpgaconvnet::verify_conv_out function for this.
-     */
+    fpgaconvnet::verify_conv_output(
+            network_parameters,
+            N,
+            &extracted_features[0],
+            "../test_data/output.bin",
+            fpgaconvnet::FORMAT_BINARY);
 
     return extracted_features;
+}
+
+
+static void load_data(std::string filename, std::vector<float> & images)
+{
+    const int image_size = 224 * 224;
+
+    images = std::vector<float>(100 * image_size * 3);
+    std::ifstream fin(filename);
+
+    if (!fin.is_open()) {
+        throw fpgaconvnet::Exception("File not found");
+    }
+
+    for (unsigned iter = 0 ; iter < N ; iter++) {
+        char byte;
+        std::vector<float> red(image_size);
+        std::vector<float> blue(image_size);
+        std::vector<float> green(image_size);
+
+        fin.read(static_cast<char*>(&byte), 1);
+
+        for (int i = 0 ; i< image_size ; i++) {
+            fin.read(static_cast<char*>(&byte), 1);
+            red[i] = float((unsigned char) byte) / 255.0;
+        }
+
+        for (int i = 0 ; i< image_size ; i++) {
+            fin.read(&byte, 1);
+            green[i] = float((unsigned char) byte) / 255.0;
+        }
+
+        for (int i = 0 ; i< image_size ; i++) {
+            fin.read(&byte, 1);
+            blue[i] = float((unsigned char) byte) / 255.0;
+        }
+
+        for (int i = 0 ; i < image_size ; i++) {
+            images[(iter * 3 * image_size) + 3 * i] = red[i];
+            images[(iter * 3 * image_size) + 3 * i + 1] = green[i];
+            images[(iter * 3 * image_size) + 3 * i + 2] = blue[i];
+        }
+    }
+
+    fin.close();
 }
 
 
@@ -62,17 +121,8 @@ int main(int argc, char **argv)
     fpgaconvnet::protos::Network network_parameters =
 	    fpgaconvnet::load_network_proto(argv[1]);
 
-    /* TODO: Code to load images here. Code below is a stub to load random
-     *       data as image input.
-     */
     std::cout << "Reading images ..." << std::endl;
-    for (unsigned i = 0 ; i < N ; i++) {
-	for (unsigned j = 0;
-		j < fpgaconvnet::calc_conv_in_size(network_parameters);
-		j++) {
-	    pixel_stream.push_back((float(rand()) / float(RAND_MAX)));
-	}
-    } 
+    load_data("../test_data/input.bin", pixel_stream);
 
     std::vector<float> conv_out = run_feature_extraction(
 	    network_parameters, pixel_stream);
