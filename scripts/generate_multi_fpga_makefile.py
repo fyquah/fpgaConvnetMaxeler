4e5c86aba90edc6450ec64ac49d37651de81e00f
Fu Yong Quah <quah.fy95@gmail.com>
2018-07-31 22:49:00 +0100

    Add bitstream id to maxfile target name

--- scripts/generate_multi_fpga_makefile.py
+++ scripts/generate_multi_fpga_makefile.py
@@ -18,14 +18,17 @@ def main():
         network = text_format.Parse(f.read(), parameters_pb2.Network())
     arr = []
     for i, layer in enumerate(network.layer):
-        if len(arr) == 0 or arr[-1] != layer.fpga_id:
-            arr.append(layer.fpga_id)
+        if layer.HasField("bitstream_id"):
+            bitstream_id = layer.bitstream_id
+        else:
+            bitstream_id = 0
+        fpga_id = layer.fpga_id
 
-        if not layer.HasField("fpga_id"):
-            raise RuntimeError("Missing fpga_id in layer %d!" % i)
+        if (bitstream_id, fpga_id) not in arr:
+            arr.append((bitstream_id, fpga_id))
 
     with open(FLAGS.output, "w") as f:
-        f.write("TARGET_NAMES= " + " ".join(["target_%d" % i for i in arr]))
+        f.write("TARGET_NAMES= " + " ".join(["target_%d_%d" % (i, j) for (i, j) in arr]))
 
 
 if __name__ == "__main__":
